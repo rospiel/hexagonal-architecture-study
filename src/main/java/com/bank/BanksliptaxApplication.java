73987f17c8d720c2c569d70f67b285de68bf0d5b
Rodrigo Santos <rospielberg@gmail.com>
2025-03-04 19:40:30 -0300

    chore.: add dependencies mapstruct, feign, validator and swagger

--- src/main/java/com/bank/BanksliptaxApplication.java
+++ src/main/java/com/bank/BanksliptaxApplication.java
@@ -2,8 +2,10 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BanksliptaxApplication {
 
 	public static void main(String[] args) {
